b3ca5e792b2fc5792a16ee6276a0c3ca174f4368
Adan William <adan.william.dev@gmail.com>
2026-09-01 20:58:16 -0300

    config

---
+++ backend/src/main/java/com/autocare/AutoCareApplication.java
@@ -0,0 +1,11 @@
+package com.autocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoCareApplication.class, args);
+    }
+}
